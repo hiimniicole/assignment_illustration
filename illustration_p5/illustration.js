c711e1c46cb7b32041af277f7179c98be4dbdd33
hiimniicole <llama11024@aol.com>
2015-10-29 15:02:50 -0400

    added mouse

--- illustration_p5/illustration.js
+++ illustration_p5/illustration.js
@@ -3,24 +3,31 @@
 var cookieImg;
 var mouseeImg;
 var backgroundImg;
-
+var cakeImg;
+var pizzaImg;
+var hotdogImg;
+var mouseWasPressed;
+//if mouse
 function preload() {
   cookeImg = loadImage("images/cookie.png");
 	mouseeImg = loadImage("images/mousee.png");
 	backgroundImg = loadImage("images/background.png");
+  cakeImg = loadImage("images/cake.png");
+  pizzaImg = loadImage("images/pizza.png");
+  hotdogImg = loadImage("images/hotdog.png");
 }
-function setup() {
-  mouseImage(img, 0, 0);
-}
+
 
 function setup() {
 	createCanvas(800, 600);
 }
 
 function draw() {
+	background(0, 0, 0);
 	//put background here
 	image(backgroundImg,0,0);
-
+	image(mouseeImg, mouseX, mouseY);
+	//ellipse(50, 50, 50, 150);
 	//if (mouseIsPressed)
 //	image
 }
